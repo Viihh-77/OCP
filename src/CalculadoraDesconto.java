b6f210c96d6fe54b77be4f70f67116c65b3b4fa1
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-02-12 16:22:03 -0300

    Chore: organizando pastas do sistema

--- src/CalculadoraDesconto.java
+++
@@ -1,6 +0,0 @@
-public class CalculadoraDesconto {
-
-    public double aplicarDesconto(Pedido pedido, EstrategiaDesconto estrategiaDesconto) {
-        return estrategiaDesconto.aplicarDesconto(pedido.getValorBruto());
-    }
-}
